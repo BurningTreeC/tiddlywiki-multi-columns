5c5a4849dd5c9eeebd8fcff8c6ca40971649f753
BurningTreeC <huber.simon@protonmail.com>
2022-04-09 21:58:20 +0200

    updates

--- plugins/tiddlywiki-multi-columns/files/modules/storyviews/multi-column.js
+++ plugins/tiddlywiki-multi-columns/files/modules/storyviews/multi-column.js
@@ -30,12 +30,12 @@ MultiColumnStoryView.prototype.navigateTo = function(historyInfo) {
 	if(!targetElement || targetElement.nodeType === Node.TEXT_NODE) {
 		return;
 	}
-	if(duration) {
-		// Scroll the node into view
-		this.listWidget.dispatchEvent({type: "tm-scroll", target: targetElement});
-	} else {
-		targetElement.scrollIntoView();
-	}
+	setTimeout(function() {
+		$tw.utils.removeClass(targetElement,"tc-navigating");
+	},duration);
+	$tw.utils.addClass(targetElement,"tc-navigating");
+	// Scroll the node into view
+	this.listWidget.dispatchEvent({type: "tm-scroll", target: targetElement});
 };
 
 MultiColumnStoryView.prototype.insert = function(widget) {
