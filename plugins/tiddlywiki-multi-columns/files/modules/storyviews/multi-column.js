1eaf7c123bded1dc03c53d2165a085eb76adf414
BurningTreeC <huber.simon@protonmail.com>
2022-04-09 20:28:04 +0200

    updates

--- plugins/tiddlywiki-multi-columns/files/modules/storyviews/multi-column.js
+++ plugins/tiddlywiki-multi-columns/files/modules/storyviews/multi-column.js
@@ -40,11 +40,46 @@ MultiColumnStoryView.prototype.navigateTo = function(historyInfo) {
 
 MultiColumnStoryView.prototype.insert = function(widget) {
 	var duration = $tw.utils.getAnimationDuration();
+	if(duration && (widget.wiki.getTiddlerText("$:/state/DisableInsertAnimation") !== "yes")) {
+		var targetElement = widget.findFirstDomNode();
+		// Abandon if the list entry isn't a DOM element (it might be a text node)
+		if(!targetElement || targetElement.nodeType === Node.TEXT_NODE) {
+			return;
+		}
+		setTimeout(function() {
+			$tw.utils.removeClass(targetElement,"tc-inserting");
+		},duration);
+		$tw.utils.addClass(targetElement,"tc-inserting");
+	}
+	if(widget.wiki.getTiddlerText("$:/state/DisableInsertAnimation") === "yes") {
+		widget.wiki.deleteTiddler("$:/state/DisableInsertAnimation");
+	}
+	if(widget.wiki.getTiddlerText("$:/state/DisableRemoveAnimation") === "yes") {
+		widget.wiki.deleteTiddler("$:/state/DisableRemoveAnimation");
+	}
 };
 
 MultiColumnStoryView.prototype.remove = function(widget) {
 	var duration = $tw.utils.getAnimationDuration();
-	widget.removeChildDomNodes();
+	if(duration && (widget.wiki.getTiddlerText("$:/state/DisableRemoveAnimation") !== "yes")) {
+		var targetElement = widget.findFirstDomNode(),
+			removeElement = function() {
+				widget.removeChildDomNodes();
+			};
+		// Abandon if the list entry isn't a DOM element (it might be a text node)
+		if(!targetElement || targetElement.nodeType === Node.TEXT_NODE) {
+			removeElement();
+			return;
+		}
+		setTimeout(removeElement,duration);
+		// Animate the closure
+		$tw.utils.addClass(targetElement,"tc-removing");
+	} else {
+		widget.removeChildDomNodes();
+	}
+	if(widget.wiki.getTiddlerText("$:/state/DisableRemoveAnimation") === "yes") {
+		widget.wiki.deleteTiddler("$:/state/DisableRemoveAnimation");
+	}
 };
 
 exports["multi-column"] = MultiColumnStoryView;
